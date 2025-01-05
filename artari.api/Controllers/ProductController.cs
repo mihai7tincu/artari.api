575ed2f45b4524ae7eeeef5f52e221df02c8b651
iirinabr <iirinabratu@gmail.com>
2025-01-05 17:24:59 +0200

    crud products

--- artari.api/Controllers/ProductController.cs
+++ artari.api/Controllers/ProductController.cs
@@ -17,10 +17,62 @@ namespace artari.api.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetAllProducts(CancellationToken cancellationToken)
+        public async Task<ActionResult<List<Product>>> GetAllProducts()
         {
-            var products = await _context.Products.Where(x => x.Id > 0).ToListAsync(cancellationToken);
+            var products = await _context.Products.Where(x => x.Id > 0).ToListAsync();
             return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProductById(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product is null)
+            {
+                return NotFound("Product not found");
+            }
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Product>>> AddProduct(Product product)
+        {
+
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Products.Where(x => x.Id > 0).ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Product>>> UpdateProduct(Product updatedProduct)
+        {
+            var dbProduct = await _context.Products.FindAsync(updatedProduct.Id);
+            if (dbProduct is null)
+            {
+                return NotFound("Product not found");
+            }
+
+            dbProduct.Name = updatedProduct.Name;
+            await _context.SaveChangesAsync();
+
+            // sa nu mai intoarca toate produsele
+            return Ok(await _context.Products.Where(x => x.Id > 0).ToListAsync());
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<List<Product>>> RemoveProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product is null)
+            {
+                return NotFound("Product not found");
+            }
+
+            _context.Products.Remove(product);
+
+            await _context.SaveChangesAsync();
+            return Ok(await _context.Products.Where(x => x.Id > 0).ToListAsync());
         }
     }
 }
