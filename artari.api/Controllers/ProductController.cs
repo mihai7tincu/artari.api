b43a3dfe0369fd35347936560a14e96e93aefb28
iirinabr <iirinabratu@gmail.com>
2025-01-06 13:54:25 +0200

    changes to post to map all product fields

--- artari.api/Controllers/ProductController.cs
+++ artari.api/Controllers/ProductController.cs
@@ -41,27 +41,46 @@ namespace artari.api.Controllers
             _context.Products.Add(product);
             await _context.SaveChangesAsync();
 
-            return Ok(await _context.Products.Where(x => x.Id > 0).ToListAsync());
+            return Ok(
+                //await _context.Products.Where(x => x.Id > 0).ToListAsync()
+                );
         }
 
         [HttpPut]
-        public async Task<ActionResult<List<Product>>> UpdateProduct(Product updatedProduct)
+        public async Task<ActionResult<Product>> UpdateProduct([FromBody]Product updatedProduct)
         {
             var dbProduct = await _context.Products.FindAsync(updatedProduct.Id);
+
             if (dbProduct is null)
             {
                 return NotFound("Product not found");
             }
 
             dbProduct.Name = updatedProduct.Name;
+            dbProduct.Description = updatedProduct.Description;
+            dbProduct.Cultivar = updatedProduct.Cultivar;
+            dbProduct.Height = updatedProduct.Height;
+            dbProduct.ImageUrl = updatedProduct.ImageUrl;
+            dbProduct.IsNew = updatedProduct.IsNew;
+            dbProduct.IsSoldout = updatedProduct.IsSoldout;
+            dbProduct.Price = updatedProduct.Price;
+            dbProduct.TypeName = updatedProduct.TypeName;
+            dbProduct.Priority = updatedProduct.Priority;
+            dbProduct.Propagation = updatedProduct.Propagation;
+            dbProduct.SpeciesName = updatedProduct.SpeciesName;
+            dbProduct.Species = updatedProduct.Species;
+            dbProduct.Type = updatedProduct.Type;
+
+            dbProduct = updatedProduct;
             await _context.SaveChangesAsync();
 
-            // sa nu mai intoarca toate produsele
-            return Ok(await _context.Products.Where(x => x.Id > 0).ToListAsync());
+            return Ok(
+                //await _context.Products.Where(x => x.Id > 0).ToListAsync()
+                );
         }
 
         [HttpDelete]
-        public async Task<ActionResult<List<Product>>> RemoveProduct(int id)
+        public async Task<ActionResult<Product>> RemoveProduct(int id)
         {
             var product = await _context.Products.FindAsync(id);
             if (product is null)
@@ -72,7 +91,9 @@ namespace artari.api.Controllers
             _context.Products.Remove(product);
 
             await _context.SaveChangesAsync();
-            return Ok(await _context.Products.Where(x => x.Id > 0).ToListAsync());
+            return Ok(
+                //await _context.Products.Where(x => x.Id > 0).ToListAsync()
+                );
         }
     }
 }
